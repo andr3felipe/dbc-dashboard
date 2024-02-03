0296f27a0307824f92ae46609346140820ad927e
unknown <Ermeson23>
2024-02-03 16:54:53 -0300

    Feat: Create login page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,9 +1,48 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { Button } from "../../components/Button";
 import * as S from "./styles";
 
 export function Login() {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+    console.log('E-mail:', email, 'Senha: ', password);
+  }
+
+  const navigateToRegister = () => {
+    navigate("/register");
+  }
+
   return (
     <S.Container>
-      <h1>Login</h1>
+      <S.LoginForm onSubmit={handleSubmit}>
+        <S.StyledTitle>Login</S.StyledTitle>
+        <S.StyledTextField
+          label="Email"
+          variant="outlined"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <S.StyledTextField
+          label="Password"
+          variant="outlined"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <Button children="Login" color="background" background="text" type="submit"></Button>
+        <S.StyledLink href="/">Esqueceu a senha?</S.StyledLink>
+        <Button children="Crie uma conta" color="background" background="text" onClick={navigateToRegister}></Button>
+      </S.LoginForm>
     </S.Container>
   );
 }
--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -1,3 +1,34 @@
+import { TextField } from "@mui/material";
+
 import styled from "styled-components";
 
-export const Container = styled.main``;
+export const Container = styled.main`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+`;
+
+export const LoginForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 40%;
+  max-width: 50%;
+  margin: auto;
+  padding: 20px;
+  gap: 1rem;
+`;
+
+export const StyledTitle = styled.h1`
+text-align: center;
+margin-bottom: 1rem;`
+
+export const StyledLink = styled.a`
+text-decoration: none;
+text-align: center;
+color: black;
+`;
+
+export const StyledTextField = styled(TextField)`
+`;

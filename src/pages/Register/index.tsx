99ed5ceaedc7c17814a0f66c5f09f75a2b6afce4
unknown <Ermeson23>
2024-02-03 18:40:54 -0300

    Feat: Create register page

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,9 +1,79 @@
+import { useForm } from 'react-hook-form'
+import { useNavigate } from "react-router-dom";
+
+import { Button } from "../../components/Button";
 import * as S from "./styles";
 
 export function Register() {
+
+  interface UserData {
+    login: string;
+    senha: string;
+  }
+
+  const { register, handleSubmit, reset } = useForm<UserData>();
+  const navigate = useNavigate();
+
+  async function registerUser(userData: UserData) {
+    try {
+        const response = await fetch("http://vemser-dbc.dbccompany.com.br:39000/vemser/pessoa-api-back/auth/create", {
+        method: "POST",
+        headers: {
+          "Content-Type": "Application/json",
+        },
+        body: JSON.stringify(userData)
+      })
+
+      if (!response.ok) {
+        const data = await response.json();
+        const { message } = data;
+
+        throw new Error(message);
+      }
+
+      // MODIFICAR
+      alert("Usuário criado com sucesso.")
+      // MODIFICAR
+      navigate("/");
+
+      const token = await response.text();
+      localStorage.setItem("token", token);
+      navigate("/");
+    }
+    catch(error) {
+      alert(error);
+    }
+  }
+
+  function onSubmit(data: UserData) {
+    registerUser(data);
+    reset();
+  }
+
+  const navigateToLogin = () => {
+    navigate("/");
+  }
+
   return (
     <S.Container>
-      <h1>Register</h1>
+      <S.LoginForm onSubmit={handleSubmit(onSubmit)}>
+        <S.StyledTitle>Cadastro</S.StyledTitle>
+        <S.StyledTextField
+          label="Login"
+          variant="outlined"
+          type="text"
+          {...register('login', { required: true })}
+        />
+        <S.StyledTextField
+          label="Senha"
+          variant="outlined"
+          type="password"
+          {...register('senha', { required: true })}
+        />
+        <Button children="Cadastrar conta" color="background" background="text" type="submit"></Button>
+        <S.StyledText>Já tem uma conta?</S.StyledText>
+        <Button children="Fazer Login" color="background" background="text" onClick={navigateToLogin}></Button>
+      </S.LoginForm>
     </S.Container>
   );
 }
--- src/pages/Register/styles.ts
+++ src/pages/Register/styles.ts
@@ -1,3 +1,33 @@
+import { TextField } from "@mui/material";
+
 import styled from "styled-components";
 
-export const Container = styled.main``;
+export const Container = styled.main`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+`;
+
+export const LoginForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 40%;
+  max-width: 50%;
+  margin: auto;
+  padding: 20px;
+  gap: 1rem;
+`;
+
+export const StyledTitle = styled.h1`
+text-align: center;
+margin-bottom: 1rem;`
+
+export const StyledText = styled.p`
+text-align: center;
+color: black;
+`;
+
+export const StyledTextField = styled(TextField)`
+`;
